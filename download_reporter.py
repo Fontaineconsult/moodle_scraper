5ee6bb83af360e7ab75422e72fb85aa00aa885f9
Daniel Fontaine <fontaineconsult@gmail.com>
2018-08-07 13:43:23 -0700

    Fixed problem with multiple appends of tracker class

--- download_reporter.py
+++ download_reporter.py
@@ -1,10 +1,10 @@
 import logging
-logging.basicConfig(filename='test.log', level=logging.INFO)
+logging.basicConfig(filename='test.log', level=logging.INFO, format='%(asctime)s: %(message)s')
 
 searched_courses = []
 
 current_course_class = None
-
+logging.info("-------------------------")
 
 class current_course:
 
@@ -16,22 +16,22 @@ class current_course:
 
 
 
-
-
 def set_current_course_log(course_id, course_name):
 
     global current_course_class
 
+    new_log_course = current_course(course_id, course_name)
+
     if current_course_class is not None:
+        print("current course is not none, appending", course_name)
         searched_courses.append(current_course_class)
 
-        current_course_class = current_course(course_id, course_name)
+        current_course_class = new_log_course
     else:
+        print("current course is none, appending", course_name)
         current_course_class = current_course(course_id, course_name)
 
-
-
-
+    print("current length", searched_courses)
 
 def test_report(report_info):
     logging.info("{} courses checked".format(str(len(report_info))))
@@ -40,17 +40,22 @@ def test_report(report_info):
 
 
 def log_resource_exists(resource):
+
     current_course_class.files_not_downloaded.append(resource)
 
 
 def log_file_download(resource):
+
     current_course_class.files_downloaded.append(resource)
 
 def log_error(short_name, link, error):
     logging.error("{} {} {}".format(short_name, link, error))
 
 def build_report():
+
+    searched_courses.append(current_course_class)
     reports = []
+    print(len(searched_courses))
     for course in searched_courses:
         files_downloaded = len(course.files_downloaded)
         files_not_downloaded = len(course.files_not_downloaded)
@@ -59,8 +64,8 @@ def build_report():
                                                                               files_not_downloaded,
                                                                               course_name)
         reports.append(report)
-    logging.info("-------------------------")
     for each in reports:
+
         logging.info(each)
 
 
